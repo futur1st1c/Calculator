b03327b458808969f0d66a13f4190156f55351e4
futur1st1c <rkmurithi2@gmail.com>
2016-09-02 19:40:10 +0300

    Removed the Js script which earlier existed while making the business Logic Interface

--- js/script.js
+++
@@ -1,7 +0,0 @@
-var add= function(number1, number2) {
-	return number1 + number2;
-};
-var number1= parseInt(prompt("Enter a Nmuber"));
-var number2= parseInt(prompt("Enter another Number"));
-
-alert(add(number1, number2));
