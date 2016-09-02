7b2324224a689595e3bbfb6e63073912e2ee0cf9
futur1st1c <rkmurithi2@gmail.com>
2016-09-02 19:19:40 +0300

    Added HTML Coding plus proper code indendation

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ var add= function(number1, number2) {
 	return number1 + number2;
 };
 var number1= parseInt(prompt("Enter a Nmuber"));
-var number2= parseInt(prompt("Enter another Nmuber"));
+var number2= parseInt(prompt("Enter another Number"));
 
 alert(add(number1, number2));
